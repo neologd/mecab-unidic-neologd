4e56dcfbf67a7523d117580917537ef34be80e30
Toshinori Sato (@overlast) <overlasting@gmail.com>
2016-01-14 15:15:13 +0900

    Delete white spaces on the head of each lines

--- libexec/git_rm_from_log_too.sh
+++ libexec/git_rm_from_log_too.sh
@@ -18,29 +18,29 @@
 
 set -e
 
- FILE_PATH=$1
- ECHO_PREFIX='[git_rm_from_history_too] '
+FILE_PATH=$1
+ECHO_PREFIX='[git_rm_from_history_too] '
 
- echo "${ECHO_PREFIX} Start."
+echo "${ECHO_PREFIX} Start."
 
- if [ ! -e ${FILE_PATH} ]; then
-     echo "${ECHO_PREFIX} ${FILE_PATH} is not found"
-     exit 1
- fi
+if [ ! -e ${FILE_PATH} ]; then
+    echo "${ECHO_PREFIX} ${FILE_PATH} is not found"
+    exit 1
+fi
 
- git pull
+git pull
 
- if [ -e ${FILE_PATH} ]; then
-     echo "${ECHO_PREFIX} git rm ${FILE_PATH}"
-     git rm ${FILE_PATH}
-     echo "${ECHO_PREFIX} git commit"
-     git commit -m "Remove obsolete file: ${FILE_PATH}"
-     echo "${ECHO_PREFIX} git push"
-     git push
-     echo "${ECHO_PREFIX} delete file from log"
-     git filter-branch -f --tree-filter "rm -f ${FILE_PATH}"
-     echo "${ECHO_PREFIX} git push --force"
-     git push --force
- fi
+if [ -e ${FILE_PATH} ]; then
+    echo "${ECHO_PREFIX} git rm ${FILE_PATH}"
+    git rm ${FILE_PATH}
+    echo "${ECHO_PREFIX} git commit"
+    git commit -m "Remove obsolete file: ${FILE_PATH}"
+    echo "${ECHO_PREFIX} git push"
+    git push
+    echo "${ECHO_PREFIX} delete file from log"
+    git filter-branch -f --tree-filter "rm -f ${FILE_PATH}"
+    echo "${ECHO_PREFIX} git push --force"
+    git push --force
+fi
 
- echo "${ECHO_PREFIX} Finish."
+echo "${ECHO_PREFIX} Finish."
